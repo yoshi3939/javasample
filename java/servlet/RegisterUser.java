03fa506d0936b7ed4a164066295419998dfcd2ef
yoshi3939 <irieyoshimi43@gmail.com>
2021-12-27 15:00:30 +0900

    フィルタ、動的インクルードについて

--- java/servlet/RegisterUser.java
+++ java/servlet/RegisterUser.java
@@ -53,7 +53,7 @@ public class RegisterUser extends HttpServlet {
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
+		//request.setCharacterEncoding("UTF-8");
 		String id = request.getParameter("id");
 		String name = request.getParameter("name");
 		String pass = request.getParameter("pass");
