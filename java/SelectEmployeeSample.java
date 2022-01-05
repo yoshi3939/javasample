1a27289978e7f8a5bd82659fb63170249e1e38ba
yoshi3939 <irieyoshimi43@gmail.com>
2022-01-05 17:43:56 +0900

    DAOパターン

--- java/SelectEmployeeSample.java
+++ java/SelectEmployeeSample.java
@@ -1,34 +1,20 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import java.util.List;
+
+import dao.EmployeeDAO;
+import model.Employee;
 
 public class SelectEmployeeSample {
 	public static void main(String[] args) {
-		//データベースに接続
-		try (Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/DB/sample", "sa", "")) {
-			
-			//SELECT文を準備
-			String sql = "SELECT ID, NAME, AGE, FROM EMPLOYEE";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-			
-			//SELECTを実行し、結果表(ResultSet)を取得
-			ResultSet rs = pStmt.executeQuery();
-			
-			//結果表に格納されたレコードの内容を表示
-			while (rs.next()) {
-				String id = rs.getString("ID");
-				String name = rs.getString("NAME");
-				int age = rs.getInt("AGE");
-				
-				//取得したデータを出力
-				System.out.println("ID:" + id);
-				System.out.println("名前:" + name);
-				System.out.println("年齢:" + age + "¥n");
-			}			
-		} catch (SQLException e) {
-			e.printStackTrace();
+		//employeeテーブルの全レコードを取得
+		EmployeeDAO empDAO = new EmployeeDAO();
+		List<Employee> empList = empDAO.findAll();
+		
+		//取得レコードの内容を出力
+		for(Employee emp : empList) {
+			System.out.println("ID:" + emp.getId());
+			System.out.println("名前:" + emp.getName());
+			System.out.println("年齢:" + emp.getAge() + "¥n");
 		}
 	}
 }
